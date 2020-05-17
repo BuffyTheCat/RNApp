9c68ab87874af73eb6698421beaa1d10cd44e961
Svyatoslav Zhuravlev <svyatoslavzhuravlev@MacBook-Pro-Svyatoslav.local>
2020-05-17 11:19:26 +0300

    add validation

--- src/components/add-todo/add-todo.js
+++ src/components/add-todo/add-todo.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import { View, StyleSheet, TextInput, Button } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, TextInput, Button, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { addTodo } from '../../actions'
 
 
 
-const AddTodo = ({ onSubmit }) => {
-    return (
-        <View style={styles.container}>
-            <TextInput style={styles.input} placeholder='Write Some Text' onChangeText={(text) => this.userName = text} />
-            <Button onPress={() => onSubmit(this.userName)} title="Add" />
-        </View>
-    )
+class AddTodo extends Component {
+    constructor(props) {
+        super(props);
+        this.inputRef = React.createRef();
+        this.state = {
+            inputText: ''
+        };
+    }
+
+    render() {
+        const { onSubmit } = this.props;
+
+        return (
+            <View style={styles.container}>
+                <TextInput ref={this.inputRef} style={styles.input} placeholder='Write Some Text' onChangeText={(text) => this.setState({inputText: text})} />
+                <Button disabled={this.state.inputText === ''} 
+                        onPress={() => {
+                            onSubmit(this.state.inputText);
+                            this.inputRef.current.clear();
+                        }} 
+                        title="Add" />
+            </View>
+        )
+    }
+
+    componentDidMount() {
+        this.inputRef.current.focus();
+    }
 }
 
 const styles = StyleSheet.create({
